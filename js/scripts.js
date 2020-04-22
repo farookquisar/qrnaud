754a1c635508e0ca859963619a67f8178021bfd6
farookquisar <59113563+farookquisar@users.noreply.github.com>
2020-04-22 15:14:36 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -375,7 +375,7 @@ function audioEndEventListnerFn (){
           if (cnt===audios.length){
                 cnt=0;
                 totPlaycnt++;
-                if (totPlaycnt>50) {
+                if (totPlaycnt>50) { 
                 totPlaycnt=0;
                 }else {
                 playAud();
