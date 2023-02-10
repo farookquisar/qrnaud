9b62875bfbf88da4bb47916665b874bb67e4e0b1
farookquisar <59113563+farookquisar@users.noreply.github.com>
2023-02-10 20:42:22 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', function (event)
+{
+    event.waitUntil(
+        caches.open('v1').then(function (cache)
+        {
+            return cache.addAll([
+                '/index.html',
+                '/manifest.json',
+                '/quraud-icon-192x192.png',
+                '/quraud-icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event)
+{
+    event.respondWith(
+        caches.match(event.request).then(function (response)
+        {
+            return response || fetch(event.request);
+        })
+    );
+});
